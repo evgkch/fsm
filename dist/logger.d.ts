b8cc4b4c0f810749694b9f8982a516b608238f4b
Evgeny Kucherenko <evg.kch@gmail.com>
2021-09-03 23:45:15 +0300

    fix: build process updated

--- dist/logger.d.ts
+++
@@ -1,8 +0,0 @@
-import { IFSM, TransitionEvent } from "/index.js";
-export declare class Logger {
-    private fsm;
-    private log;
-    pointers: Set<any>;
-    constructor(fsm: IFSM<any, any, any>, log: (evt: TransitionEvent<any, any>) => void);
-    terminate(): void;
-}
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,2 @@
-mkdir -p dist/@lib/emitterjs
-cp node_modules/@lib/emitterjs/dist/index.d.ts dist/@lib/emitterjs
 tsc
+web-crack
